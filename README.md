49bbe8d9ee9c7d23627d8a8b81d52388218ea5ef
Andea Sosso <andrea@sosso.me>
2012-11-30 19:56:53 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# Ambiente di sviluppo con Vagrant #
+
+## Installazione e aggiornamenti ##
+
+Puoi scaricare tutti i file di vagrant con il comando:
+
+	git clone --recursive git@github.com:iAndre89/vagrant.git
+
+#### Aggiornamento submoduli ####
+Per aggiornare i submoduli collegati:
+
+	git pull origin master
+	git submodule init
+	git submodule update
+
+Se vuoi forzare l'aggiornamento di ciascun submodulo, anche se non è tracciato nel repository principale:
+
+	git submodule foreach git pull origin master
+        
+## File hosts ##
+
+#### Linux / MacOS X (Automatico) 
+Installare vagrant hostmaster con il comando
+
+	vagrant gem install vagrant-hostmaster
+
+Sostituire il file *Vagrantfile* con *Vagrantfile.LinuxHost*
+
+#### Linux / MacOS X (Manuale) 
+Modificare il file /etc/hosts ad esempio con 
+
+	sudo nano /etc/hosts 
+
+e inserire il seguente virtualhost:
+
+	127.0.0.1	pma.dev
+
+#### Windows (solo manuale)
+Aggiungere alla file: *C:\Windows\System32\Driver\etc\hosts*
+
+	127.0.0.1	pma.dev
+
+## Avviare la macchina virtuale
+Controlla di avere [virtualbox](http://download.virtualbox.org/virtualbox/4.2.4/ "Virtualbox 4.2.4") e lanciare vagrant con il comando:
+
+	vagrant up
+		
+Questo comando esegue molte operazioni:
+
+* Scarica il sistema operativo
+* Installa il sistema operativo su una macchina virtualbox
+* Avvia la macchina virtuale
+* Provisioning dell'ambiente tramite puppet
+
+Per avviare la macchina virtuale senza il provisioning di puppet usare il comando
+
+	vagrant up --no-provision
+
+## Supporto NFS (Linux / MacOS X)
+La condivisione delle cartelle nativa di Dropox è decisamente lenta rispetto al Network File System (NFS)
+
+Una volta che nfs-utils è stato installato nella macchina guest è possibile abilitare l'NFS nel vagrantfile:
+
+Commentando 
+
+	# config.vm.share_folder("v-data", "/home/vagrant/data", "data", :nfs => false)
+
+Decommentando
+
+	config.vm.share_folder("v-data", "/home/vagrant/data", "data", :nfs => true, :extra => 'dmode=777,fmode=777')
+	
+Per semplicit&agrave; si pu&ograve; sostituire *Vagrantfile* con *VagrantFile.LinuxHostNFS*
+
+## Accesso a phpMyAdmin
+* [http://pma.dev/](http://pma.dev/ "pma.dev")
---
+++ manifests/project.pp
@@ -0,0 +1,135 @@
+Exec { 
+	path => ['/usr/local/bin/:/bin/:/usr/bin/'],
+}
+
+### Gestione dell'utente 
+user { 'vagrant':
+	ensure	=> present,
+	groups	=> ['nginx'],
+	require	=> Package['nginx'],
+}
+
+file { "/home/vagrant/tmp":
+    ensure	=> "directory",
+    mode	=> 777,
+}
+
+file { "/home/vagrant/logs":
+    ensure	=> "directory",
+    mode	=> 700,
+}
+
+file { "/home/vagrant":
+    ensure	=> "directory",
+    mode	=> 755,
+}
+
+### YUM e repository
+class { 'yum':
+	 extrarepo => ['nginx', 'remi', 'remi-test', 'epel'],	 
+}
+
+### Nginx
+class { "nginx": 
+	require	=> Class['yum::repo::nginx'],
+}
+
+### Mysql
+class { "mysql": }
+
+### phpMyAdmin
+package {'phpMyAdmin': 
+	ensure  => latest,
+	require	=> Class['yum::repo::remi'],
+}
+
+file { '/etc/nginx/conf.d/pma.conf':
+	ensure	=> file,
+	mode	=> 600,
+	source	=> 'puppet:///modules/tekarea/pma.conf',
+	require	=> [Package['phpMyAdmin'], Package['nginx']],
+	notify  => Service["nginx"], 
+}
+
+file { '/etc/phpMyAdmin/config.inc.php':
+	ensure	=> file,
+	mode	=> 644,
+	source	=> 'puppet:///modules/tekarea/config.inc.php',
+	require	=> Package['phpMyAdmin'],
+}
+
+### PHP-FPM
+package {'php-fpm':
+	ensure	=> latest, 
+	require	=> Class['yum::repo::remi'],
+}
+
+package { ["php-common", "php", "php-pecl-apc", "php-cli", "php-pear", "php-mysqlnd", "php-pdo", "php-gd", "php-mbstring", "php-xml", "php-imap", "php-mcrypt", "mcrypt", "php-intl", "php-devel"]: 
+	ensure => latest,
+	require	=> [Package['php-fpm'], Class['yum::repo::epel']],
+}
+
+service { 'php-fpm': 
+	 enable => true,
+	 ensure => "running",
+	 require	=> Package['php-fpm'],
+}
+
+file { '/etc/php-fpm.d/vagrant.pool.conf':
+	ensure	=> file,
+	mode	=> 600,
+	source	=> 'puppet:///modules/tekarea/vagrant.pool.conf',
+	require	=> Package['php-fpm'],
+	notify  => Service["php-fpm"], 
+}
+
+file { '/etc/php.ini':
+	ensure	=> file,
+	mode	=> 600,
+	source	=> 'puppet:///modules/tekarea/php.ini',
+	require	=> Package['php-fpm'],
+	notify  => Service["php-fpm"], 
+}
+
+### Network File Sistem
+package {'nfs-utils':
+	ensure  => latest,
+}
+
+### Pacchetti utili
+package {['nano', 'yum-utils', 'mlocate', 'git', 'curl']:
+	ensure => latest, 
+}
+
+### Java (utilizzato da assetic)
+package {'java-1.7.0-openjdk':
+	ensure	=> latest,
+	require	=> Class['yum'],
+}
+
+### Installazione di composer.phar nella cartella /bin
+exec { "install_composer":
+    command => "curl -s https://getcomposer.org/installer | php -- --install-dir=/bin",
+    require => [Package['curl'], Package['php']],
+    creates => "/bin/composer.phar",
+}
+
+### Messaggio di benvenuto
+host {'self':
+	ensure => present,
+	name => $fqdn,
+	host_aliases => ['tekarea.dev', $hostname], ip => $ipaddress,
+}
+
+file {'motd':
+	ensure => file,
+	path => '/etc/motd',
+	mode => 0644,
+	content => "\n*** Tekarea development OS ***\n\nNome macchina: ${hostname}\nSistema operativo: ${operatingsystem}\nDominio: ${domain}\n\n\n",
+}
+
+### Aggiorno l'indice di ricerca
+exec { "refresh_locate":
+    command => "updatedb",
+    require	=> Package['mlocate'],
+}
