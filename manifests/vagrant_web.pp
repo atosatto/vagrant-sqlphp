d3d29ceff10800cdd25b247716cd5b4ee17022f6
Andrea Sosso <andrea@sosso.me>
2013-06-24 09:11:58 +0200

    xdebug 4 vagrant

--- manifests/vagrant_web.pp
+++ manifests/vagrant_web.pp
@@ -94,7 +94,7 @@ package {'php-fpm':
 	require	=> Class['yum::repo::remi'],
 }
 
-package { ["php-common", "php", "php-pecl-apc", "php-cli", "php-pear", "php-mysqlnd", "php-pdo", "php-gd", "php-mbstring", "php-xml", "php-imap", "php-mcrypt", "mcrypt", "php-intl", "php-devel", "php-soap"]: 
+package { ["php-common", "php", "php-pecl-apc", "php-pecl-xdebug", "php-cli", "php-pear", "php-mysqlnd", "php-pdo", "php-gd", "php-mbstring", "php-xml", "php-imap", "php-mcrypt", "mcrypt", "php-intl", "php-devel", "php-soap"]: 
 	ensure => installed,
 	require	=> [Package['php-fpm'], Class['yum::repo::epel']],
 }
